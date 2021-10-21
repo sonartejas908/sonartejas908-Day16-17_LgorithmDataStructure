e92079d5bd52e3746187481d815f66153af11c5a
Tejassonar <sonartejas908@gmail.com>
2021-10-21 13:36:41 +0530

    Added main method of execution

---
+++ Algorithm_Problems/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day16_and_17Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Permutation_of_string.StringPermutation();
+            BubbleSort.UserInput();
+            Insertion_Sort_Num.sort();
+            Insertion_sort_Char.sort();
+            Anagram_Detection.DetectAnagram();
+
+        }
+    }
+}
