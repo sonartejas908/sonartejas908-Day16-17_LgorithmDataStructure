dd45dd30230ee498c4c7dcb808a09ebfde140ce5
Tejassonar <sonartejas908@gmail.com>
2021-10-21 13:49:32 +0530

    Added main method for Data structure

---
+++ Data_Structure_Problems/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Data_Structure_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack.CheckParentheses();
+            Primt_Range.PrimeRange();
+        }
+    }
+}
